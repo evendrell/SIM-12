2c8e9da6af988dc3b87f79aee30571d864cf4182
sergioutrero <sergio.utrero@estudiantat.upc.edu>
2023-12-27 20:35:05 +0100

    queue

--- Code/llibreria/queue.py
+++ Code/llibreria/queue.py
@@ -1,12 +1,26 @@
 from slam import *
+from queue import Queue
+from entitat import *
 
-class nopActivity(slamiii):
+class queue(slamiii):
     entren=0
-     # conèixer el motor de simulació pot anar molt bé
+    # conèixer el motor de simulació pot anar molt bé
     def __init__(self,scheduler,parametres):
-        super(nopActivity, self).__init__(scheduler,parametres)
+        super(queue, self).__init__(scheduler,parametres)
         #Que faig amb parametres, en aquest cas nop
         self.parametres=parametres
+        lista_atributos = parametres.split(',')
+        #Esther t'he corregit l'accés als paràmetres dons els dos primers són l'identificador de l'activitat i el tipus activitat
+        #Atenció tots, recordeu a forçar el tipus del paràmetre
+        self.cap = int(lista_atributos[2])
+        self.m = int(lista_atributos[3])
+        self.set_estat(Estat.LLIURE)
+        #Esther, al final no necessita la cua per emmagatzemar temporalment les entitats pq sols usem el LAST com a métode d'assignació dels atributs de la nova entitat
+        #self.cola = Queue(maxsize=self.m)
+        self.novaEntitat = None
+        self.entitatsProcesades = 0
+        self.estadisticProcessades=0
+        self.estadisticCreades=0
         
     def __repr__(self):
         return "nop "+str(self.id())
@@ -19,23 +33,20 @@ class nopActivity(slamiii):
         pass      
 
     def iniciSimulacio(self):
-        super(nopActivity, self).iniciSimulacio()
-        self.entren=0
-        self.set_estat(Estat.LLIURE)
-        print('Soc nop i he rebut un iniciSimulacio')
-        #Jo no he de fer res
+        #TODO la classe pare manega el successor
+        super(queue, self).iniciSimulacio()
+        #TODO posar els estadístics a zero (nombre entitats processades, nombre d'entitas empaquetades)
         pass
     
     def fiSimulacio(self):
-        #Aquí tampoc faig res
-        pass
+        super(queue,self).fiSimulacio()
  
     def acceptaEntitat(self, n):
         #aquí estic suposant que ho accepto tot, us convenç?
         return n
     
     def summary(self):
-        #Pot ser una bona praxis disposar d'un resum del que ha fet el vostre element al llarg de tota l'execució
-        return " EST: "+str(self.entren)+' '+str(self._surten)
+        #Mostrar els estadístics per pantalla amb el nom, format desitjat 
+        return " EST: "+str(self.estadisticProcessades)+' '+str(self.estadisticCreades)
         
         
