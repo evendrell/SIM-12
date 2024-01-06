fd5e398656a864fd34dffc994f5566c0d6afcf38
Mario <meriog2001@gmail.com>
2024-01-06 11:40:45 +0100

    Select creado

---
+++ Code/llibreria/select.py
@@ -0,0 +1,29 @@
+from slam import *
+
+class select(slamiii):
+
+    
+   
+
+
+
+    #Código chorizeado
+    def iniciSimulacio(self):
+        super(nopActivity, self).iniciSimulacio()
+        self.entren=0
+        self.set_estat(Estat.LLIURE)
+        print('Soc nop i he rebut un iniciSimulacio')
+        #Jo no he de fer res
+        pass
+    
+    def fiSimulacio(self):
+        #Aquí tampoc faig res
+        pass
+ 
+    def acceptaEntitat(self, n):
+        #aquí estic suposant que ho accepto tot, us convenç?
+        return n
+    
+    def summary(self):
+        #Pot ser una bona praxis disposar d'un resum del que ha fet el vostre element al llarg de tota l'execució
+        return " EST: "+str(self.entren)+' '+str(self._surten)
