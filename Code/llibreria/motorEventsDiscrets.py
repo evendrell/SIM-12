541b18d33b5fdb7f10c04a2a17f776030ddf3795
danielmr6 <dmoronroces@gmail.com>
2024-01-10 22:49:15 +0100

    [HOTFIX] Model updated and fixed execution for gate and move

--- Code/llibreria/motorEventsDiscrets.py
+++ Code/llibreria/motorEventsDiscrets.py
@@ -168,10 +168,6 @@ class motorEventsDiscrets:
         if 'move' in activitat:
             creat = True
             element = move(self, activitat)
-            # element.set_gate_reference(global_gate_reference)
-            element.set_gate_reference(self._llistaActivitats[0])
-            print("Desde la cració del move rescato la ref.: " + str(self._llistaActivitats[0]))
-            global_gate_reference = None
         if not creat:
             element=nopActivity(self,activitat)
         
@@ -188,37 +184,30 @@ class motorEventsDiscrets:
             #self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[0],0,TipusEvent.TraspasEntitat,entitat(),0,0))
             #self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[0],10,TipusEvent.TraspasEntitat,entitat(),0,0))
             #self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[0],20,TipusEvent.TraspasEntitat,entitat(),0,0))
-            # nopInstance = nopActivity(self, "nop,0")
 
             entities = [entitat() for i in range(3, 10)]
 
             #Abrimos puerta
-            self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[0], 0, TipusEvent.ObrirPorta, entities[4],self._llistaActivitats[0],0))
+            self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[4], 0, TipusEvent.ObrirPorta, entities[4],self._llistaActivitats[4],0))
 
             #Puerta abierta, traspasamos entidades a Gate y Move
-            self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[0],0,TipusEvent.TraspasEntitat,entities[0],self._llistaActivitats[2]))
+            self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[4],0,TipusEvent.TraspasEntitat,entities[0],self._llistaActivitats[6]))
 
-            self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[1],5,TipusEvent.TraspasEntitat,entities[1],self._llistaActivitats[2]))
+            self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[5],5,TipusEvent.TraspasEntitat,entities[1],self._llistaActivitats[6]))
                 # Move --> Gate open? --> Gate open --> Move --> gate(entity)
 
             #Cerramos puerta
-            self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[0], 10, TipusEvent.TancarPorta, entities[5],self._llistaActivitats[0]))
+            self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[5], 10, TipusEvent.TancarPorta, entities[5],self._llistaActivitats[4]))
 
             #Puerta cerrada, traspasamos entidades a Move
-            self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[1], 15, TipusEvent.TraspasEntitat, entities[2],self._llistaActivitats[2]))
+            self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[5], 15, TipusEvent.TraspasEntitat, entities[2],self._llistaActivitats[6]))
                 # Move --> Gate open?
                         #--> Gate closed
 
                 # Move pasan T ticks (4t) --> Move deja pasar la entidad
                 #la puerta esta cerrada, las entidades se mueren
 
-            # self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[0],15,TipusEvent.ObrirPortaEnTTics,entitat(),self._llistaActivitats[0]))
-            # self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[0],20,TipusEvent.TraspasEntitat,entitat(),nopInstance))
-            # self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[0],5,TipusEvent.EstaLaPortaOberta,entitat(),nopInstance))
-            # self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[0],30,TipusEvent.TancarPorta,entitat(),nopInstance))
-
-            # self.afegirEsdeveniment(
-            #     esdeveniment(self._llistaActivitats[0], 0, TipusEvent.TraspasEntitat, entitat(), nopInstance, 0))
+            # self.afegirEsdeveniment(esdeveniment(self._llistaActivitats[5],15,TipusEvent.ObrirPortaEnTTics,entitat(),self._llistaActivitats[4]))
     def fiSimulacio(self):
         for activitat in self._llistaActivitats:
             activitat.fiSimulacio()
